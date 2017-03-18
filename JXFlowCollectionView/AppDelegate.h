7dbb909bec9439f3024161d35219a14d52c7194b
rookieJX <wangjiaxiangyx@163.com>
2017-03-18 09:35:12 +0800

    自定义瀑布流
    初始化文件提交

---
+++ JXFlowCollectionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JXFlowCollectionView
+//
+//  Created by 王加祥 on 2017/3/18.
+//  Copyright © 2017年 王加祥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JXFlowCollectionView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JXFlowCollectionView
+//
+//  Created by 王加祥 on 2017/3/18.
+//  Copyright © 2017年 王加祥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
